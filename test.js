10a281feb5cddcccca6abf713d8c5b8d55823a31
JeanG1914 <jeanpaulgzea@gmail.com>
2023-04-12 22:28:16 +0000

    working 100%

--- test.js
+++ test.js
@@ -22,29 +22,29 @@ test("One euro should be 1.206 dollars", function(){
     // this is the comparison for the unit test
      expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
 })
-test("One euro should be 1.206 dollars", function(){
+test("One dollar should be 127.9 yen", function(){
     //import the function from app.js
     const { fromDollarToYen } = require('./app.js')
 
     // use the function like its suppoed to be used
-    const yen = fromDollarToYen(3.5)
+    const yen = fromDollarToYen(1)
 
-    // if 1 euro are 1.206 dollars, then 3.5 euros should be (3.5 * 1.2)
-    const expected = 3.5 * 1.2; 
+    // if 1 dollar are 127.9 yen, then 1 dollars should be (1 * 127.9)
+    const expected = 1 * 127.9; 
     
     // this is the comparison for the unit test
-     expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
+     expect(fromDollarToYen(1)).toBe(127.9); //1 dollar are 127.9 yen, then 1 dollars should be = (1 * 127.9)
 })
-test("One euro should be 1.206 dollars", function(){
+test("One yen should be 0.8 pounds", function(){
     //import the function from app.js
-    const { fromEuroToDollar } = require('./app.js')
+    const { fromYenToPound } = require('./app.js')
 
     // use the function like its suppoed to be used
-    const dollars = fromEuroToDollar(3.5)
+    const pounds = fromYenToPound(100)
 
-    // if 1 euro are 1.206 dollars, then 3.5 euros should be (3.5 * 1.2)
-    const expected = 3.5 * 1.2; 
+    // if 1 yen are 0.8 pounds, then 100 yens should be (100 * 0.8)
+    const expected = 100 * 0.8; 
     
     // this is the comparison for the unit test
-     expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
+     expect(fromYenToPound(100)).toBe(80); //1 yen are 0.8 pounds, then 100 yens should be = (100 * 0.8)
 })
